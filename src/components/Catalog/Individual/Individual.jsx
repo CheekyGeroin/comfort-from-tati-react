4e2d1de82f20056ca636015e9f4570a7c122a038
Danylo Altukhov <dan.97198@gmail.com>
2023-02-07 21:26:34 +0200

    add forms

--- src/components/Catalog/Individual/Individual.jsx
+++ src/components/Catalog/Individual/Individual.jsx
@@ -1,4 +1,106 @@
+import { Container, Form, FormContainer, Input, Item, Label, List, Option, Select, SelectContainer, SelectItem, SelectList, Textarea, Title, TitleContainer } from "./Individual.styled";
+
 const Individual = () => {
-    
-}
-export default Individual
+  return (
+    <Container>
+      <SelectContainer>
+        <TitleContainer>
+          <Title>Найменування товару</Title>
+        </TitleContainer>
+        <Form>
+          <SelectList>
+            <SelectItem>
+              <Label>
+                Категорія товару
+                <Select name="category">
+                  <Option value="-"> </Option>
+                  <Option value="plaids">Великі пледи</Option>
+                  <Option value="kids">Дитячі пледи</Option>
+                  <Option value="bathroom">Килим у ванну</Option>
+                </Select>
+              </Label>
+            </SelectItem>
+            <SelectItem>
+              <Label>
+                Плетіння
+                <Select name="type">
+                  <Option value="-"> </Option>
+                  <Option value="heart">Серденько</Option>
+                  <Option value="honey">Соти</Option>
+                  <Option value="braided">Плетінка</Option>
+                  <Option value="kids">Дитячі</Option>
+                  <Option value="bath">Килим у ванну</Option>
+                </Select>
+              </Label>
+            </SelectItem>
+            <SelectItem>
+              <Label>
+                Розмір
+                <Select name="size">
+                  <Option value="-"> </Option>
+                  <Option value="huge">Великий</Option>
+                  <Option value="medium">Середній</Option>
+                  <Option value="small">Маленький</Option>
+                </Select>
+              </Label>
+            </SelectItem>
+            <SelectItem>
+              <Label>
+                Колір
+                <Select name="colour">
+                  <Option value="-"> </Option>
+                  <Option value="black">Чорний</Option>
+                  <Option value="white">Білий</Option>
+                </Select>
+              </Label>
+            </SelectItem>
+            <SelectItem>
+              <Label>
+                Побажання
+                <Textarea cols="30" rows="10"></Textarea>
+              </Label>
+            </SelectItem>
+          </SelectList>
+        </Form>
+      </SelectContainer>
+      <FormContainer>
+        <TitleContainer>
+          <Title>Особисті дані</Title>
+        </TitleContainer>
+        <Form>
+          <List>
+            <Item>
+              <Label>
+                Ім’я та прізвище
+                <Input type="text" name="name" />
+              </Label>
+            </Item>
+            <Item>
+              <Label>
+                Номер телефону
+                <Input type="tel" name="phone" />
+              </Label>
+            </Item>
+            <Item>
+              <Label>
+                Нік в телеграмі
+                <Input type="text" name="nickname" />
+              </Label>
+            </Item>
+          </List>
+        </Form>
+      </FormContainer>
+      <div>
+        <ul>
+          <li>
+            <button></button>
+          </li>
+          <li>
+            <button></button>
+          </li>
+        </ul>
+      </div>
+    </Container>
+  );
+};
+export default Individual;
---
+++ src/components/Catalog/Individual/Individual.styled.jsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+export const Container = styled.div``
+
+export const SelectContainer = styled.div``
+
+export const TitleContainer = styled.div``
+
+export const Title = styled.h2``
+
+export const Form = styled.form``
+
+export const SelectList = styled.ul``
+
+export const SelectItem = styled.li``
+
+export const FormContainer = styled.div``
+
+export const List = styled.ul``
+
+export const Item = styled.li``
+
+export const Label = styled.label``
+
+export const Select = styled.select``
+
+export const Option = styled.option``
+
+export const Input = styled.input``
+
+export const Textarea= styled.textarea``
+
+export const Link = styled(NavLink)`
+`
